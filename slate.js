89a9272be284c70b62086a1c75f91c9b29b5b40a
yurei <167486448+yurei0903@users.noreply.github.com>
2025-10-06 16:56:33 +0900

    クラスの名前を定数にしました

--- slate.js
+++ slate.js
@@ -3,29 +3,30 @@ const body=document.getElementsByTagName('body')[0];
 console.log(stoneclass.length);
 //石板の大きさを変更する関数
 
-
+const stoneclass_name='stone';
+const nohover_class_name='stone_nohover';
+const buckbotan='backbutton';
 function stoneclick(event)
     {
-        console.log("Sotone clicked");
         let bigstone=event.target;
         
         for (let j = 0; j < stoneclass.length; j++) {
             stoneclass[j].style.display='none';
         }
         bigstone.style.display='block';
-        bigstone.classList.replace('stone','stone_nohover');
+        bigstone.classList.replace(stoneclass_name,nohover_class_name);
         body.style.margin='1% 5% 1% 5%';
 
         const backButton = document.createElement('button');
-        backButton.className = 'backbutton';
-        backButton.id = 'backbutton';
+        backButton.className = buckbotan;
+        backButton.id = buckbotan;
         backButton.textContent = '戻る';
 
         body.appendChild(backButton);
-        let backbutton=document.getElementById('backbutton');
+        let backbutton=document.getElementById(buckbotan);
         backbutton.addEventListener("click", function() {
             
-            bigstone.classList.replace('stone_nohover','stone');
+            bigstone.classList.replace(nohover_class_name,stoneclass_name);
             body.style.margin='3% 10% 0% 10%';
 
             for (let j = 0; j < stoneclass.length; j++) {
